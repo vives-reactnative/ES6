d926e8ee3c27cdc796a297e53b5122dc3779da84
Yves Seurynck <yves.seurynck@vives.be>
2019-10-02 16:33:29 +0200

    update

--- Block Scoped Declarations/opdrachten/const.js
+++ Block Scoped Declarations/opdrachten/const.js
@@ -2,7 +2,7 @@
  *
  * Consider renaming to const convention
  * */
-const x = 1;
+var x = 1;
 x = 2; // should raise a TypeError when changed to const
 
 console.log(x); // returns 2 if nothing is changed
